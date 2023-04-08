d4473a26a173338b4327334bc47f58100b22f6e6
Alper <hayrettinalpersapan@gmail.com>
2023-04-08 14:57:57 +0300

    denemeLevel4

--- GameJam/Assets/Scripts/FirstLevel/Level1Hareket.cs
+++ GameJam/Assets/Scripts/FirstLevel/Level1Hareket.cs
@@ -8,6 +8,18 @@ public class Level1Hareket : MonoBehaviour
     float horizontal;
     float vertical;
     [SerializeField] float speed;
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    
+    void ChangeSprite(Sprite spriteChange)
+    {
+        spriteRenderer.sprite = spriteChange; 
+    }
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,4 +32,14 @@ public class Level1Hareket : MonoBehaviour
     {
         transform.Translate(horizontal * Time.fixedDeltaTime * speed , vertical * Time.fixedDeltaTime * speed , transform.position.z);    
     }
+    
+           
+    void OnCollisionEnter2D(Collision2D donus)
+    {
+         if (donus.gameObject.CompareTag("Donus"))
+        {
+           Debug.Log("carptin");
+           ChangeSprite(newSprite);
+        } 
+    } 
 }
