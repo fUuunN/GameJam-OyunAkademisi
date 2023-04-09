4faafe560f458d8ade7708d931bf2fde5d6b2d6a
Alper <hayrettinalpersapan@gmail.com>
2023-04-09 18:49:45 +0300

    level5 yapma cabaları canvas bozuk

--- GameJam/Assets/Scenes/AhmetFolder/HealthBar.cs
+++ GameJam/Assets/Scenes/AhmetFolder/HealthBar.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour
 {
-    
+    public GameObject greenBar;
+    private float health=0;
     // Start is called before the first frame update
     void Start()
     {
-        int sayi = Collectibles.score;
+        health = (Collectibles.score)*10;
     }
 
     // Update is called once per frame
