b33ab9c20452b54dba3ad6d5f16c6b261335eea1
fUuunN <95917480+fUuunN@users.noreply.github.com>
2023-04-09 15:45:17 +0300

    collectible değişiklikleri

--- GameJam/Assets/Scenes/AhmetFolder/Collectibles.cs
+++ GameJam/Assets/Scenes/AhmetFolder/Collectibles.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Collectibles : MonoBehaviour
 {
     static int score;
+    public TMP_Text text;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Collectible"))
         {
             score++;
             Debug.Log("Toplandý");
-            Destroy(gameObject);
+            text.text = "Puan : " + score;
+            Destroy(collision.gameObject);
+            
         }
     }
 }
