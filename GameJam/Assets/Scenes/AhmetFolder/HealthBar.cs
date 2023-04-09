9592844f87d5033ac6a790ffe1834615c8ae8383
Alper <hayrettinalpersapan@gmail.com>
2023-04-10 00:09:17 +0300

    healthbar

--- GameJam/Assets/Scenes/AhmetFolder/HealthBar.cs
+++ GameJam/Assets/Scenes/AhmetFolder/HealthBar.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public GameObject greenBar;
-    private float health=0;
+    public Image greenBar;
+    public float health;
+    //float bar
     // Start is called before the first frame update
     void Start()
     {
         health = (Collectibles.score)*10;
+       // greenBar.transform.scale.x
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+        
     }
+
+    void OnTriggerEnter2D(Collider2D engel)
+    {
+         if (engel.gameObject.CompareTag("engel"))
+        {
+           Debug.Log("carptin");
+           health-=10;
+        } 
+    } 
 }
