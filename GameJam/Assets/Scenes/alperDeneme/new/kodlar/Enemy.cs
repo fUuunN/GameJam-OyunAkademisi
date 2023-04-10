663e86fa5bd3ba6ab422b59aba8b9ba08525037c
Alper <hayrettinalpersapan@gmail.com>
2023-04-10 04:10:36 +0300

    animasyonlar ve enemy deneme

---
+++ GameJam/Assets/Scenes/alperDeneme/new/kodlar/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator anim;
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        anim.SetTrigger("Hurt");
+
+        if (currentHealth<=0)
+        {
+            Die();
+        }
+        void Die()
+        {
+
+            anim.SetBool("IsDead", true);
+
+            this.enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            Destroy(gameObject, 2f);
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ GameJam/Assets/Scenes/alperDeneme/new/kodlar/PlayerCombat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+
+
+    public void DamageEnemy()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            //Debug.Log("Zarar" + enemy.name);
+
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint==null)
+            return;
+
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+
+
+
+
+}
